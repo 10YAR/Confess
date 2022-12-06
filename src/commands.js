1eff99f51b9fe4b745eb5000e5a4294821d66121
Diyar <diyar@digiplace.fr>
2022-12-06 15:29:33 +0100

    Help commands

--- src/commands.js
+++ src/commands.js
@@ -1,5 +1,13 @@
 export const commands = [
     {
+        name: 'help',
+        description: 'List of commands',
+    },
+    {
+        name: 'invite',
+        description: 'Gives invite link of the bot',
+    },
+    {
         name: 'setchannel',
         description: 'Sets the channel for the bot to send messages in',
     },
--- src/index.js
+++ src/index.js
@@ -6,12 +6,46 @@ import { randomEmoji } from "../libs/random-emoji.js";
 client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
+    if (interaction.commandName === 'invite') {
+        if (interaction.channel.type === 1) {
+            try {
+                interaction.reply('https://discord.com/api/oauth2/authorize?client_id=796832150853779456&permissions=19456&redirect_uri=https%3A%2F%2Fconfess.diyar.dev&response_type=code&scope=messages.read%20applications.commands%20bot')
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }
+
+
+    if (interaction.commandName === 'help') {
+
+        const helpMsgEmbed = new EmbedBuilder()
+            .setTitle('Confess Bot')
+            .setDescription('Confess Bot is a bot that allows you to confess anonymously!')
+            .addFields(
+                {name: '--------------------------', value: '**Commands :**'},
+                {name: '```/help```', value: 'Help command for the bot'},
+                {name: '```/setchannel```', value: 'Sets the channel for the bot to send messages in'},
+                {name: '```/confess```', value: 'Allows you to confess! (Use this only in DMs)'},
+                {name: '```/invite```', value: 'Invite link for the bot (Use this only in DMs)'},
+                {name: '--------------------------', value:'Make sure to enable the "Allow direct messages from server members" option in your settings!'},
+                {name: 'Need more help ?', value: 'contact@diyar.dev or 10YAR#0001'}
+            );
+        try {
+            interaction.reply({embeds: [helpMsgEmbed]});
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
     if (interaction.commandName === 'setchannel') {
         if (await hasMessagePermission(interaction.channel)) {
             await setChannel(interaction.guild.id, interaction.guild.name, interaction.channel.id);
-            interaction.reply('Channel set!');
-        } else {
-            console.log("No permission to send messages in this channel :(");
+            try {
+                interaction.reply('Channel set!');
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 
@@ -28,13 +62,11 @@ client.on('interactionCreate', async interaction => {
                     // STEP 2 : Select a server & Confirm Confession
                     interaction.channel.awaitMessages({max: 1, time: 900000, errors: ['time']})
                         .then(async collected => {
-                            //interaction.deferReply('Getting server list...');
 
                             const guilds = [];
                             for (const [, guild] of interaction.client.guilds.cache) {
                                 await guild.members.fetch(interaction.user).then(() => guilds.push(guild)).catch(error => console.log(error));
                             }
-
 
                             const selectServers = [];
                             for (let i = 0; i < Object.keys(guilds).length; i++) {
@@ -53,7 +85,11 @@ client.on('interactionCreate', async interaction => {
                                         .addOptions(...selectServers)
                                 );
 
-                            await interaction.followUp({embeds: [serverSelectEmbed], components: [selectAction]});
+                            try {
+                                await interaction.followUp({embeds: [serverSelectEmbed], components: [selectAction]});
+                            } catch (error) {
+                                console.error(error);
+                            }
 
                             // STEP 4 : Sending and Saving Confession
                             interaction.channel.awaitMessageComponent({componentType: 3, time: 15000})
@@ -76,24 +112,45 @@ client.on('interactionCreate', async interaction => {
                                                     .setDescription(confession);
                                                 await serverChannel.send({embeds: [confessionEmbed]});
                                                 await saveConfession(confession, interaction.user.id, interaction.user.username, interaction.user.discriminator, guildId, 'SENT');
-                                                await interaction.followUp('Confession sent! \nCheck it out : ' + serverChannel.toString());
+
+                                                try {
+                                                    await interaction.followUp('Confession sent! \nCheck it out : ' + serverChannel.toString());
+                                                } catch (error) {
+                                                    console.error(error);
+                                                }
                                             } else {
                                                 await saveConfession(confession, interaction.user.id, interaction.user.username, interaction.user.discriminator, guildId, 'ERROR');
-                                                await interaction.followUp('Error: Channel not found');
+                                                try {
+                                                    await interaction.followUp('Error: Channel not found');
+                                                } catch (error) {
+                                                    console.error(error);
+                                                }
                                             }
                                         }
                                     } else {
-                                        await interaction.followUp({content: 'Error: This server is not configured yet! Please contact their admins.'});
+                                        try {
+                                            await interaction.followUp({content: 'Error: This server is not configured yet! Please contact their admins.'});
+                                        } catch (error) {
+                                            console.error(error);
+                                        }
                                     }
                                 });
                         })
                         .catch(collected => {
-                            interaction.followUp('Error, type the /confess command again please.');
+                            try {
+                                interaction.followUp('Error, type the /confess command again please.');
+                            } catch (error) {
+                                console.error(error);
+                            }
                         });
 
                 });
         } else {
-            interaction.channel.send("You can only use this command in DMs!");
+            try {
+                interaction.channel.send("You can only use this command in DMs!");
+            } catch (error) {
+                console.error(error);
+            }
         }
     }
 });
@@ -116,24 +173,32 @@ async function hasMessagePermission(channel) {
 }
 
 async function setChannel(guildId, guildName, channelId) {
-    db.query("SELECT * FROM servers WHERE guild_id = ?", [guildId], (err, result) => {
-        if (result[0] == null) {
-            db.query("INSERT INTO servers (guild_id, guild_name, channel_id) VALUES ('" + guildId + "', '" + guildName + "', '" + channelId + "')", function (err, result, fields) {
-                if (err) throw err;
-                console.log("Channel set! (" + guildId + ") " + guildName + " - (" + channelId + ")");
-            });
-        }else {
-            db.query("UPDATE servers SET channel_id = '" + channelId + "' WHERE guild_id = '" + guildId + "'", function (err, result, fields) {
-                if (err) throw err;
-                console.log("Channel updated! (" + guildId + ") " + guildName + " - (" + channelId + ")");
-            });
-        }
-    });
+    try {
+        db.query("SELECT * FROM servers WHERE guild_id = ?", [guildId], (err, result) => {
+            if (result[0] == null) {
+                db.query("INSERT INTO servers (guild_id, guild_name, channel_id) VALUES ('" + guildId + "', '" + guildName + "', '" + channelId + "')", function (err, result, fields) {
+                    if (err) throw err;
+                    console.log("Channel set! (" + guildId + ") " + guildName + " - (" + channelId + ")");
+                });
+            } else {
+                db.query("UPDATE servers SET channel_id = '" + channelId + "' WHERE guild_id = '" + guildId + "'", function (err, result, fields) {
+                    if (err) throw err;
+                    console.log("Channel updated! (" + guildId + ") " + guildName + " - (" + channelId + ")");
+                });
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 async function saveConfession(confession, userId, username, discriminator, guildId, status = 'SENT') {
-    db.query("INSERT INTO confessions (confession, user_id, username, guild_id, status) VALUES ('" + confession + "', '" + userId + "', '" + username + "#" + discriminator + "', '" + guildId + "', '" + status + "')", function (err, result, fields) {
-        if (err) throw err;
-        console.log("Confession saved! (SERVER: " + guildId + ") - (USER: " + userId + ") " + username + "#" + discriminator + "(" + status + ")");
-    });
+    try {
+        db.query("INSERT INTO confessions (confession, user_id, username, guild_id, status) VALUES ('" + confession + "', '" + userId + "', '" + username + "#" + discriminator + "', '" + guildId + "', '" + status + "')", function (err, result, fields) {
+            if (err) throw err;
+            console.log("Confession saved! (SERVER: " + guildId + ") - (USER: " + userId + ") " + username + "#" + discriminator + "(" + status + ")");
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
